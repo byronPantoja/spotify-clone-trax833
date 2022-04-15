74be517838c1ae807cbc5cca7f11d3c17359287e
developer833 <byronpantoja@gmail.com>
2022-04-16 06:26:47 +0800

    adding sidebar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,43 +1,42 @@
-import type { AppProps } from "next/app";
-import { ChakraProvider, extendTheme } from "@chakra-ui/react";
-import "reset-css";
-import PlayerLayout from "../components/playerLayout";
+import { ChakraProvider, extendTheme } from '@chakra-ui/react'
+import 'reset-css'
+import PlayerLayout from '../components/playerLayout'
 
 const theme = extendTheme({
   colors: {
     gray: {
-      100: "#f5f5f5",
-      200: "#eeeeee",
-      300: "#e0e0e0",
-      400: "#bdbdbd",
-      500: "#9e9e9e",
-      600: "#757575",
-      700: "#616161",
-      800: "#424242",
-      900: "#212121",
+      100: '#f5f5f5',
+      200: '#eeeeee',
+      300: '#e0e0e0',
+      400: '#bdbdbd',
+      500: '#9e9e9e',
+      600: '#757575',
+      700: '#616161',
+      800: '#424242',
+      900: '#212121',
     },
   },
   components: {
     Button: {
       variants: {
         link: {
-          ":focus": {
-            outline: "none",
-            boxShadow: "none",
+          ':focus': {
+            outline: 'none',
+            boxShadow: 'none',
           },
         },
       },
     },
   },
-});
-const MyApp = ({ Component, pageProps }: AppProps) => {
+})
+const MyApp = ({ Component, pageProps }) => {
   return (
     <ChakraProvider theme={theme}>
       <PlayerLayout>
-        <Component {...pageProps} />;
+        <Component {...pageProps} />
       </PlayerLayout>
     </ChakraProvider>
-  );
-};
+  )
+}
 
-export default MyApp;
+export default MyApp
