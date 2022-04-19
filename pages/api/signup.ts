f11a5467cb8cc0cfdad6bc1cf2ebc33358f34b45
developer833 <byronpantoja@gmail.com>
2022-04-19 11:38:09 +0800

    trying to fix HTTP/1.1 401 error

--- pages/api/signup.ts
+++ pages/api/signup.ts
@@ -1,9 +1,8 @@
-import { User } from './../../../node_modules/.prisma/client/index.d'
 import bcrypt from 'bcrypt'
 import jwt from 'jsonwebtoken'
 import cookie from 'cookie'
 import { NextApiRequest, NextApiResponse } from 'next'
-import prisma from '../../../lib/prisma'
+import prisma from '../../lib/prisma'
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   const salt = bcrypt.genSaltSync()
